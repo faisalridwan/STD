3e6a6afce33f1055b7ce8949079815d9d76cd5bf
Faisal Ridwan Siregar <31500226+faisalridwan@users.noreply.github.com>
2018-02-04 17:10:42 +0700

    Add files via upload

---
+++ TP/TP2/main.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+#include "tp2.h"
+
+//using namespace std;
+
+int main() {
+
+    List L;
+    address P = NULL;
+    creatList(L);
+    int i=0;
+    while(true) {
+        insertFirst(L, alokasi(5));
+        deleteFirst(L, P);
+        dealokasi(P);
+        //printInfo(L);
+
+       // cout << i;
+    }
+    return 0;
+}
---
+++ TP/TP2/tp2.cpp
@@ -0,0 +1,61 @@
+#include "tp2.h"
+
+void creatList(List L) {
+    first(L) = NULL;
+}
+
+address alokasi(infotype info){
+    address P =  new elmlist;
+    info(P) = info;
+    next(P) = NULL ;
+    return P;
+}
+address dealokasi(address &P) {
+    delete P;
+}
+
+void printInfo(List L) {
+    address curr =first(L);
+    while(curr){
+        cout <<info(curr)<<"( " << curr <<" ),";
+        curr= next(curr);
+    }
+}
+void insertFirst (List &L, address P) {
+    if(first(L)){
+        first(L) = P ;
+   }else{
+        next(P) = first(L);
+        first(L) = P;
+    }
+}
+void deleteFirst(List &L, address P){
+    if (first(L) != NULL) {
+        P = first(L);
+        first(L) = next(P);
+        next(P) = NULL;
+    }
+}
+ void reverseList(List &L) {
+    address curr =first(L);
+    int l=0;
+
+    while(next(curr)) {
+        l++;
+        curr = next(curr);
+    }
+
+    for(int i=0; i<l;i++) {
+        address f = first(L);
+        address endd = first(L);
+
+        for(int j=0;j<l-l;j++) {
+            endd = next(endd);
+        }
+
+        first(L) = next(first(L));
+        next(f) = next(endd);
+        next(endd) = f;
+    }
+ }
+
