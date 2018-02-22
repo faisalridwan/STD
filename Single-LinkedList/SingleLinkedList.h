0c1488ede3f4da3dc59a23e15669a9a195755d0a
Faisal Ridwan Siregar <31500226+faisalridwan@users.noreply.github.com>
2018-02-22 16:28:14 +0700

    Add files via upload

---
+++ Single-LinkedList/SingleLinkedList.h
@@ -0,0 +1,35 @@
+#ifndef SINGLELINKEDLIST_H_INCLUDED
+#define SINGLELINKEDLIST_H_INCLUDED
+
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+using namespace std;
+
+typedef int infotype;
+typedef struct elmList *address;
+
+struct elmList{
+    infotype info;
+    address next;
+};
+struct singleList{
+    address first;
+};
+void MenuInsert(singleList &L, address &P);
+void MenuDelete(singleList &L, address &P);
+void MenuUtama(singleList &L, address &P);
+void creatList(singleList &L);
+address alokasi(infotype info);
+address dealokasi(address &P);
+void printInfo(singleList L);
+void insertFirstSingle(singleList &L, address P);
+void insertLastSingle(singleList &L, address P);
+void insertAfterSingle(singleList &L, address P, infotype prec);
+void deleteFirstSingle(singleList &L, address P);
+void deleteLastSingle(singleList &L, address P);
+void deleteAfterSingle(singleList &L,address P, infotype prec );
+
+
+
+#endif // SINGLELINKEDLIST_H_INCLUDED
