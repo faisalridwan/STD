bf8abc7407a067e4583ee31c89b250ed9a44752b
Faisal Ridwan Siregar <31500226+faisalridwan@users.noreply.github.com>
2018-02-22 15:58:20 +0700

    Add files via upload

---
+++ Single-LinkedList/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include "SingleLinkedList.h"
+/* Created BY : FAISAL RIDWAN SIREGAR */
+using namespace std;
+
+int main()
+{
+    singleList L;
+    creatList(L);
+    address P;
+    infotype prec;
+    MenuUtama(L,P);
+    return 0;
+}
+/* Created BY : FAISAL RIDWAN SIREGAR */
