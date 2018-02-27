e16edd475402db84b89fdff89d72f613049f4e32
Faisal Ridwan Siregar <31500226+faisalridwan@users.noreply.github.com>
2018-02-27 22:54:46 +0700

    Add files via upload

---
+++ Single-LinkedList/Double-LinkedList/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "DoubleLinkedList.h"
+using namespace std;
+
+int main()
+{
+    doubleList L;
+    creatList(L);
+    address P;
+    infotype prec;
+    MenuUtama(L,P);
+    return 0;
+}
